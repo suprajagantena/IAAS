a8e692a700de9df42d6d34d9349b9702c0e6ad04
suprajagantena <suprajagantena@gmail.com>
2020-01-11 19:20:26 -0500

    Update linux.tf

--- linux.tf
+++ linux.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-062f7200baf2fa504"
   instance_type = "t2.micro"
+  key_name      = "jenkinskeypair"
 
   tags = {
     Name = "My Instance"
